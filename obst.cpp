f97407659a4b28d0d963f2820526759299e95f01
Mosin Inamdar <100260513+MosinInamdar@users.noreply.github.com>
2023-06-07 19:31:21 +0530

    Update obst.cpp

--- obst.cpp
+++ obst.cpp
@@ -1,5 +1,4 @@
-/*Mr. Sujit A. Ahirrao
-Sandip Institute of Engineering & Management
+/*
 Given sequence k = k1 <k2 < ... < kn of n sorted keys, with a search probability 
 pi  for  each  key  ki  .  Build  the  Binary  search  tree  that  has  the  least  search  cost  given  the  access 
 probability for each key. 
