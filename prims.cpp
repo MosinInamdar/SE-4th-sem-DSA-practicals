3d3b16bc329c84b700460036b9dac8920a97508b
Mosin Inamdar <100260513+MosinInamdar@users.noreply.github.com>
2023-06-07 19:31:37 +0530

    Update prims.cpp

--- prims.cpp
+++ prims.cpp
@@ -1,6 +1,4 @@
 /*
-Author: Parag Ghorpade
-
 PROBLEM STATEMENT:
 	You have a business with several offices; you want to lease phone lines to connect them up
 	with each other; and the phone company charges different amounts of money to connect
